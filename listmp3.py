02e52cdb9a2b7ac81e22fe828cc07a7fde05c114
Simon Caine <simon@offshoreweather.com.au>
2019-03-31 16:37:44 +1100

    added basic check to determine what OS we are using

--- listmp3.py
+++ listmp3.py
@@ -1,1 +1,16 @@
+import os
+from sys import platform
+
 print("list mp3s")
+
+if platform == "linux" or platform == "linux2":
+    print('determined that we are running on linux')
+    # linux
+elif platform == "darwin":
+    # OS X
+    print('determined that we are running on Mac OSX')
+elif platform == "win32":
+    # Windows...
+    print('determined that we are running on WinBlows')
+
+

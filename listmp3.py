72457b4afbe8ddb125268a296c37dcda931c577b
Nic Nolan <nnolan2k@gmail.com>
2019-03-31 17:11:44 +1100

    added find mp3 function

--- listmp3.py
+++ listmp3.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 from sys import platform
 
 print("list mp3s")
@@ -13,4 +14,17 @@ elif platform == "win32":
     # Windows...
     print('determined that we are running on WinBlows')
 
+def findmp3s (basedir):
+    allmp3s = glob (basedir + "*.mp3")
+    return allmp3s
 
+
+base_dir = "E://Media/Music/" 
+mymp3s = findmp3s (base_dir)
+print(f"found {len(mymp3s)}")
+for mp3 in mymp3s:
+    print(f'found the mp3 {mp3}')
+
+
+
+print('the program has ended')

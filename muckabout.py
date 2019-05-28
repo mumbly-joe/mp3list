98c4c0d4e0f1ba81ec0d61eb6047d9f54b5e8197
Simon Caine <simon@offshoreweather.com.au>
2019-05-28 21:16:50 +1000

    added more printing of stuff

--- muckabout.py
+++ muckabout.py
@@ -1,5 +1,15 @@
-
-
-greeting = 'Hello You'
+greeting = "Hello You"
 print(greeting)
 print(type(greeting))
+
+my_int = 1
+print(f"the integer is {my_int}")
+print(f"the type of the integer is {type(my_int)}")
+
+my_float = 1.0
+print(f"the float is {my_float}")
+print(f"the type of the float is {type(my_float)}")
+
+my_float2 = 0.999999999999
+print(f"this is another float: {my_float2}")
+print(f"it is still of the samme type {type(my_float2)}")

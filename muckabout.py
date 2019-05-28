1504a0fb51ee3fc3ae090c788d3e64905cde189f
Simon Caine <simon@offshoreweather.com.au>
2019-05-28 21:24:57 +1000

    added some stuf fabout lists

--- muckabout.py
+++ muckabout.py
@@ -13,3 +13,32 @@ print(f"the type of the float is {type(my_float)}")
 my_float2 = 0.999999999999
 print(f"this is another float: {my_float2}")
 print(f"it is still of the samme type {type(my_float2)}")
+
+# this is a very useful data type called a list
+
+
+print("now we can muck with lists")
+# create an empty list
+my_list = []
+
+# to add to the list you can do
+my_list.append("my_string")
+# add something else
+my_list.append(10)
+# add more stuff
+my_list.append(20.234)
+
+# we can print from the list
+
+# print the first position because we start from 0
+print(my_list[0])
+
+# print the 3rd position
+print(my_list[2])
+
+print("now we will do a for loop")
+for my_value in my_list:
+    print(my_value)
+    if my_value == 10:
+        print("the value is 10! ")
+

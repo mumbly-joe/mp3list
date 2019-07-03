4fe66164bc30f0b7098909b7138cf06b96a35f3b
Nic Nolan <nnolan2k@gmail.com>
2019-07-03 22:16:14 +1000

    utils functioning ok

--- utils.py
+++ utils.py
@@ -7,9 +7,16 @@ def listdirs():
         for subdirname in dirnames:
             print(os.path.join(dirpath, subdirname))
 
-def musicfile():
-
-    for dirpath, dirnames, filenames in os.walk('E://Media/Music/'):
-      # print path to all filenames.
+def musicfile(basedir):
+    filelist=[]
+    for dirpath, dirnames, filenames in os.walk(basedir): #eg. basedir= E://Media/Music/
+        # print path to all filenames.
         for filename in filenames:
-            print(os.path.join(dirpath, filename))
+            filelist.append(os.path.join(dirpath, filename))
+    return filelist
+
+def savelist(mylist):
+    fid=open('list.txt', 'w', encoding='utf-8')
+    for myfile in mylist:
+        fid.writelines(myfile+'\n')
+    fid.close()
